279f4b2929369960383f7f3e08f65768f7e361d5
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2025-08-19 09:54:38 +0300

    Create Module1 and tasks

---
+++ Module1/src/module_1_1/Task2.java
@@ -0,0 +1,11 @@
+package module_1_1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("    *\n" +
+                         "   ***\n" +
+                         "  *****\n" +
+                         " *******\n" +
+                         " ");
+    }
+}
