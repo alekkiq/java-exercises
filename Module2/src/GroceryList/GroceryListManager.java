816bca94aedce1acb08ba2b3d0dc13bd2df30050
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2025-08-24 17:23:27 +0300

    Add Module 2.2 task 2 version of GroceryListManager

--- Module2/src/GroceryList/GroceryListManager.java
+++ Module2/src/GroceryList/GroceryListManager.java
@@ -1,13 +1,16 @@
 package GroceryList;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GroceryListManager {
-    private ArrayList<String> groceryList = new ArrayList<>();
+    // private ArrayList<String> groceryList = new ArrayList<>();
+    private Map<String, Double> groceryList = new HashMap<>();
 
-    void addItem(String item) {
-        if (!groceryList.contains(item)) {
-            groceryList.add(item);
+    void addItem(String item, Double cost) {
+        if (!groceryList.containsKey(item)) {
+            groceryList.put(item, cost);
         }
     }
 
@@ -15,29 +18,39 @@ public class GroceryListManager {
         groceryList.remove(item);
     }
 
+    boolean checkItem(String item) {
+        return groceryList.containsKey(item);
+    }
+
     void displayList() {
         int idx = 1; // easier to read indexing
-        for (String s : groceryList) {
-            System.out.println(idx + ". " + s);
+        for (Map.Entry<String, Double> item : groceryList.entrySet()) {
+            System.out.println(idx + ". " + item.getKey() + ": " + item.getValue());
             idx++;
         }
     }
 
-    boolean checkItem(String item) {
-        return groceryList.contains(item);
+    double calculateTotalCost() {
+        double total = 0.0;
+        for (Map.Entry<String, Double> item : groceryList.entrySet()) {
+            total += item.getValue();
+        }
+        return total;
     }
 
     public static void main(String[] args) {
         GroceryListManager groceryList = new GroceryListManager();
 
-        groceryList.addItem("Apples");
-        groceryList.addItem("Milk");
-        groceryList.addItem("Bread");
+        groceryList.addItem("Apples", 5.99);
+        groceryList.addItem("Milk", 1.95);
+        groceryList.addItem("Bread", 3.00);
 
         System.out.println("Grocery List:");
         groceryList.displayList();
 
-        System.out.println("\nIs Milk in the grocery lit? " + groceryList.checkItem("Milk"));
+        System.out.printf("\nTotal Cost: %.2f", groceryList.calculateTotalCost());
+
+        System.out.println("\nIs Milk in the grocery list? " + groceryList.checkItem("Milk"));
 
         System.out.println("\nRemoving Milk from the list...");
         groceryList.removeItem("Milk");
